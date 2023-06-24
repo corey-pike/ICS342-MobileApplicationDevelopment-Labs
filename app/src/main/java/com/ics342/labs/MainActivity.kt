07cf2aa233b4d79dfd3ad8a20da921d2fb2d229e
Corey <jz6142gb@go.minnstate.edu>
2023-06-24 10:02:43 -0500

    lab4 commit

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -3,17 +3,24 @@ package com.ics342.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.ics342.labs.data.DataItem
 import com.ics342.labs.ui.theme.LabsTheme
 import androidx.compose.ui.text.font.FontWeight
@@ -58,16 +65,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun DataItemView(dataItem: DataItem) {
-    Column(modifier = Modifier.padding(16.dp)) {
+    val dialogState = remember { mutableStateOf(false) }
+    Column(modifier = Modifier.padding(16.dp).clickable { dialogState.value = true }) {
         Row(modifier = Modifier.fillMaxWidth()) {
             Text(text = "${dataItem.id}", fontSize = 40.sp, fontWeight = FontWeight.Bold)
             Column(modifier = Modifier.padding(start = 10.dp, top = 5.dp)) {
@@ -75,7 +75,22 @@ fun DataItemView(dataItem: DataItem) {
                 Text(text = dataItem.description)
             }
         }
+        if (dialogState.value) {
+            AlertDialog(
+                onDismissRequest = { dialogState.value = false },
+                title = { Text(text = dataItem.name) },
+                text = { Text(text = dataItem.description) },
+                confirmButton = {
+                    Button(
+                        onClick = { dialogState.value = false },
+                    ) {
+                        Text(text = "Okay")
+                    }
+                }
+            )
+        }
     }
+    Modifier.clickable { dialogState.value = true }
 }
 
 @Composable
