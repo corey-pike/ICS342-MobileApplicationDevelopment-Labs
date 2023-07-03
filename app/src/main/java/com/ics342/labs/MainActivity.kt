d8a39a16f0c1f95a4bebae2d8301d06f0c5945d6
Corey <jz6142gb@go.minnstate.edu>
2023-07-03 18:38:56 -0500

    Lab5 commit

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+<<<<<<< Updated upstream
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,17 +16,37 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+=======
+import androidx.compose.foundation.layout.*
+>>>>>>> Stashed changes
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+<<<<<<< Updated upstream
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+=======
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+>>>>>>> Stashed changes
 import com.ics342.labs.data.DataItem
 import com.ics342.labs.ui.theme.LabsTheme
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 private val dataItems = listOf(
     DataItem(1, "Item 1", "Description 1"),
@@ -55,20 +76,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LabsTheme {
-                // A surface container using the 'background' color from the theme
+                val navController = rememberNavController()
+                val viewModel: DetailsScreenViewModel = ViewModelProvider(this)[DetailsScreenViewModel::class.java]
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    DataItemList(dataItems)
+                    val selectedDataItem by viewModel.selectedDataItem.observeAsState()
+                    NavHost(navController = navController, startDestination = "dataItemList") {
+                        composable("dataItemList") {
+                            DataItemList(dataItems) { dataItem ->
+                                viewModel.setSelectedDataItem(dataItem)
+                                navController.navigate("detailsScreen")
+                            }
+                        }
+                        composable("detailsScreen") {
+                            DetailsScreen(selectedDataItem ?: DataItem(0, "", ""), navController)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+<<<<<<< Updated upstream
 @Composable
 fun DataItemView(dataItem: DataItem) {
     val dialogState = remember { mutableStateOf(false) }
     Column(modifier = Modifier.padding(16.dp).clickable { dialogState.value = true }) {
         Row(modifier = Modifier.fillMaxWidth()) {
+=======
+class DetailsScreenViewModel : ViewModel() {
+    private val _selectedDataItem = MutableLiveData<DataItem>()
+    val selectedDataItem: LiveData<DataItem> = _selectedDataItem
+    fun setSelectedDataItem(dataItem: DataItem) {
+        _selectedDataItem.value = dataItem
+    }
+}
+
+@Composable
+fun DataItemView(dataItem: DataItem, onItemClick: (DataItem) -> Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onItemClick(dataItem) },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+>>>>>>> Stashed changes
             Text(text = "${dataItem.id}", fontSize = 40.sp, fontWeight = FontWeight.Bold)
             Column(modifier = Modifier.padding(start = 10.dp, top = 5.dp)) {
                 Text(text = dataItem.name, fontWeight = FontWeight.Bold)
@@ -92,21 +145,11 @@ fun DataItemView(dataItem: DataItem) {
     }
     Modifier.clickable { dialogState.value = true }
 }
-
 @Composable
-fun DataItemList(dataItems: List<DataItem>) {
-    /* Create the list here. This function will call DataItemView() */
+fun DataItemList(dataItems: List<DataItem>, onItemClick: (DataItem) -> Unit) {
     LazyColumn {
         itemsIndexed(dataItems) { _, dataItem ->
-            DataItemView(dataItem)
+            DataItemView(dataItem, onItemClick)
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LabsTheme {
-        DataItemList(dataItems)
     }
 }
