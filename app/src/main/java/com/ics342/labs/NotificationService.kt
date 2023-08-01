8f8334eeead7f1149fba9a486755d110c9ec99f7
Corey <jz6142gb@go.minnstate.edu>
2023-07-31 21:51:17 -0500

    Initial commit lab8

--- app/src/main/java/com/ics342/labs/NotificationService.kt
+++ app/src/main/java/com/ics342/labs/NotificationService.kt
@@ -3,28 +3,28 @@ package com.ics342.labs
 import android.Manifest.permission
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
-import java.util.UUID
 
-class NotificationService: Service() {
+class NotificationService : Service() {
 
-    private val notificationManager: NotificationManagerCompat =
-        NotificationManagerCompat.from(this)
+    private lateinit var notificationManager: NotificationManagerCompat
 
     override fun onCreate() {
         super.onCreate()
-        createNotificationChannel()
+        notificationManager = NotificationManagerCompat.from(this)
+        createNotificationChannel(this)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // If permission has not been granted, stop the service and return from
-        // onStartCommand
         if (ContextCompat.checkSelfPermission(
                 this@NotificationService,
                 permission.POST_NOTIFICATIONS
@@ -34,23 +34,45 @@ class NotificationService: Service() {
             return START_NOT_STICKY
         }
 
-       // Build notification
+        val smallIcon = R.drawable.star
 
-        TODO("Build and show notification")
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Notification Title")
+            .setContentText("This is the notification content")
+            .setSmallIcon(smallIcon)
+            .setContentIntent(createNotificationIntent())
+            .build()
+
+        startForeground(NOTIFICATION_ID, notification)
+        notificationManager.notify(NOTIFICATION_ID, notification)
         return START_STICKY_COMPATIBILITY
     }
 
+    private fun createNotificationIntent(): PendingIntent {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        return PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+    }
     override fun onBind(intent: Intent?): IBinder? {
         // No need to implement for lab 8
         return null
     }
 
-    private fun createNotificationChannel() {
-        TODO("Create notification channel and register with the system")
+    private fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Labs8"
+            val channelDescription = "Description for Labs8"
+            val notificationChannel = NotificationChannel(CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = channelDescription
+                enableLights(true)
+                enableVibration(true)
+            }
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
     }
 
     companion object {
-        private const val CHANNEL_ID = "LAB_7_CHANNEL_ID"
+        private const val CHANNEL_ID = "LABS8_CHANNEL_ID"
         private const val NOTIFICATION_ID = 1234
     }
 }
